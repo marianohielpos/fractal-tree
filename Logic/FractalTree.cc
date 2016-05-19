38d07cf6ff183fb99ae6bac4b4a9531bd700ff39
Rorri Datos <rorridatos@localhost.localdomain>
2016-05-18 22:26:23 -0300

    Arbol B+

--- Logic/FractalTree.cc
+++ Logic/FractalTree.cc
@@ -16,8 +16,450 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#include "./f-tree.h"
-#include "./node.h"
-
+#include <iostream.h>
+#include <fstream.h>
+#include "../Logic/FractalTree.h"
+#include "../Common/Node.h"
 #include "../DataAccess/Persistance.h"
+
+// Metodo refrescar
+void FractalTree::refrescar(){
+	nivel = Altura ();
+	Node *aux = raiz;
+
+	while (!arbolVacio(aux->ramas[0]))
+		aux= aux->ramas[0];
+
+	vsam = aux;
+}
+
+// Metodo de Insertar
+void FractalTree::insertar (NodoProducto clave){
+	ea = false;
+	empujar (clave, raiz);
+	if(ea){
+		Node *nuevo = new Node();
+		nuevo->cuentas = 1;
+		nuevo->claves[1] = mediana;
+		nuevo->ramas [0] = raiz;
+		nuevo->ramas [1] = Xr;
+		raiz = nuevo;
+	}
+	refrescar();
+}
+
+// Metodo Empujar
+void FractalTree::empujar (NodoProducto clave, Node *nodo){
+	if(nodo == NULL){
+		ea= true;
+		mediana= clave;
+		Xr= NULL;
+	}
+	else{
+		esta = buscarNodo (clave, nodo);
+		if(esta)
+			cout << "\nCodigo Repetido: "<< clave.Codigo << endl;
+		else{
+			empujar (clave, nodo->ramas[k]);
+			buscarNodo(clave, nodo);
+
+			if(ea){
+				if(nodo->cuentas < MAX){
+					//cout << "Voy a meterhoja!" <<endl;
+					ea = false;
+					P = nodo;
+					// Usa las variables globales
+					meterHoja();
+				}
+				else{
+					//cout << "Voy a dividir!" << endl;
+					ea = true;
+					P = nodo;
+					// Usa las variables globales.
+					dividir();
+				}
+			}
+		}
+	}
+}
+
+// Metodo Meter Hoja que usa las variables globales: Mediana, P, K, Xr
+void FractalTree::meterHoja(){
+	for(int i = P->cuentas; i >= k + 1; i--){
+			P->claves[i + 1] = P->claves[i];
+			P->ramas [i + 1] = P->ramas [i];
+	}
+	P->claves[k + 1] = mediana;
+	P->ramas [k + 1] = Xr;
+	P->cuentas++;
+}
+
+// Metodo Dividir Nodo que usa las variables asignadas por Meter Hoja Mediana, P, K, Xr.
+void FractalTree::dividir(){
+	Node *mde = new Node();
+	int pos;
+
+	if (k <= MIN)
+		pos= MIN;
+	else
+		pos= MIN+1;
+
+	for(int i= pos+1; i <= MAX; i++){
+		mde->claves[i-pos] = P->claves[i];
+		mde->ramas [i-pos] = P->ramas [i];
+	}
+
+	mde->cuentas = MAX-pos;
+	P->cuentas = pos;
+
+	if (k <= MIN)
+		meterHoja();
+	else{
+		k = k-pos;
+		Node *aux = P;
+		P = mde;
+		meterHoja();
+		P = aux;
+	}
+	mediana = P->claves[P->cuentas];
+
+	if(arbolVacio(P->ramas[0]))
+		doblar(mde);
+
+	mde->ramas[0] = P->ramas[P->cuentas];
+	mde->shojas = P->shojas;
+	P->shojas = mde;
+
+	P->cuentas--;
+	Xr= mde;
+}
+
+// Metodo que duplica un Nodo
+void FractalTree::doblar (Node *mde){
+	for(int i = mde->cuentas; i >= 1; i--){
+		mde->claves[i + 1] = mde->claves[i];
+		mde->ramas [i + 1] = mde->ramas [i];
+	}
+	mde->claves[ 1] = mediana;
+	mde->cuentas++;
+}
+
+// Metodo Arbol Vacio
+bool FractalTree::arbolVacio(Node *nodo){
+    return ((nodo == NULL) || (nodo->cuentas == 0));
+}
+
+// Metodo buscar que retorna si el nodo existe o no
+bool FractalTree::buscar(NodoProducto clave){
+	return buscarNodo (clave, raiz);
+}
+
+// Metodo buscar que retorna si el nodo existe o no
+bool FractalTree::buscar(NodoProducto clave, Node *nodo){
+	if(nodo->cuentas == 0)
+		esta = false;
+	else{
+		buscarNodo(clave, nodo);
+		if(esta)
+			N= nodo;
+		else
+			esta = buscar(clave, nodo->ramas[k]);
+	}
+	return esta;
+}
+
+// Metodo buscar nodo
+bool FractalTree::buscarNodo(NodoProducto clave, Node *nodo){
+        if(nodo == NULL)
+        	return false;
+        else{
+			if(clave.Codigo < nodo->claves[1].Codigo){
+					k = 0;
+					return esta = false;
+			}
+			else{
+				k = nodo->cuentas;
+				while((k > 1) && (clave.Codigo <nodo->claves[k].Codigo))
+					k--;
+
+				esta = (clave.Codigo == nodo->claves[k].Codigo);
+			}
+			return esta;
+        }
+}
+
+// Realiza un Imprimir Recursivo
+void FractalTree::print_rec (){
+    if (!arbolVacio(raiz)){
+        print_rec_aux(raiz);
+        cout << endl;
+    }
+    else
+    	cout << "\nEl Arbol B+ esta Vacio!" << endl;
+}
+
+// Metodo auxiliar de print_rec
+void FractalTree::print_rec_aux (Node *nodo){
+	if(!arbolVacio(nodo)){
+		print_rec_aux (nodo->ramas[0]);
+		for(int i = 1; i <= nodo->cuentas; i++){
+			cout << nodo->claves[i].Codigo << " ";
+			print_rec_aux (nodo->ramas[i]);
+		}
+	}
+}
+
+// Metodo que retorna la Altura del arbol B+
+int FractalTree::Altura (){
+	int res = 0;
+	Node *aux = raiz;
+	if (arbolVacio(aux)){
+		cout << "\nEl Arbol B+ esta Vacio!" << endl;
+		return 0;
+	}
+	else{
+		while (!arbolVacio(aux)){
+			res++;
+			aux = aux->ramas[0];
+		}
+		return res;
+	}
+}
+
+
+// Realiza el recorrido en Anchura
+void FractalTree:: Anchura(){
+	int largo = Altura();
+	int con = 1;
+	Node *pg = raiz;
+	cout<<"Impresion del arbol B+ en anchura:"<<endl;
+	Anchura_aux (pg, largo, con);
+	cout << endl;
+}
+
+// Metodo auxiliar de Anchura
+void FractalTree:: Anchura_aux (Node *nodo, int largo, int con){
+	if (arbolVacio(nodo)){
+	}
+	else{
+		if (largo >= con){
+			ImprimeNivel (nodo, con);
+			con++;
+			Anchura_aux(nodo,largo, con);
+		}
+	}
+}
+
+void FractalTree::ImprimeNivel (Node *nodo, int con){
+	if(arbolVacio(nodo)){
+	}
+	else{
+		if (con == 1){
+			for(int i= 1; i<= nodo->cuentas; i++){
+				cout<<nodo->claves[i].Codigo<<" ";
+			}
+		}
+		else{
+			con--;
+			for(int r= 0; r <= nodo->cuentas; r++)
+				ImprimeNivel(nodo->ramas[r], con);
+		}
+	}
+}
+
+// Metodo Imprimir Arbol B+
+void FractalTree ::Imprimir (){
+	Node *aux= vsam;
+	if (arbolVacio(aux))
+		cout << "\nEl Arbol B+ esta Vacio!" << endl;
+	else{
+		cout<<"Impresion del arbol B+:"<<endl;
+		while (!arbolVacio(aux)){
+			for (int i= 1; i <= aux->cuentas; i++)
+				cout<<aux->claves[i].Codigo<<" ";
+			aux = aux->shojas;
+		}
+	}
+	cout << endl;
+}
+
+/****************************/
+
+bool FractalTree::Buscar(int c){
+    int i;
+
+    Node *nodo = vsam;
+    while (nodo != NULL){
+        i = 0;
+
+        for(; i <= (nodo->cuentas); i++){
+            if (nodo->claves[i].Codigo == c){
+                return  true;
+            }
+        }
+        nodo = (*nodo).shojas;
+    }
+    return false;
+}
+
+//ve si el el codigo c existe
+int FractalTree:: Esta(int c){
+    int i;
+    Node *nodo = vsam;
+    while (nodo != NULL){
+        i = 0;
+        for(; i <= ((nodo->cuentas) - 1); i++){
+            if (nodo->claves[i].Codigo == c)
+            return  true;
+        }
+        nodo = (*nodo).shojas;
+    }
+    return false;
+}
+
+//encuentra y retorna el nodo buscado
+NodoProducto* FractalTree:: Encontrar(int c){
+    int i;
+    Node *nodo = vsam;
+    NodoProducto *siesta;
+    while (nodo != NULL){
+        i = 0;
+
+        for(; i <= (nodo->cuentas); i++){
+            if (nodo->claves[i].Codigo == c){
+                siesta = new NodoProducto (nodo->claves[i].Codigo ,
+				nodo->claves[i].Precio, nodo->claves[i].Descripcion);/////ojo
+                return  siesta;
+            }
+        }
+        nodo = (*nodo).shojas;
+    }
+    return NULL;
+}
+
+/*******************************************************************************************/
+/*******************************************************************************************/
+
+void FractalTree::preOrdenRecursivo(Node *raiz){
+	preOrdenRecursivoAux(raiz);
+}
+
+/* Realiza un recorrido del arbol de forma iterativa */
+void FractalTree::preOrdenRecursivoAux(Node *raiz){
+
+	Node *pg = raiz;
+
+	if( raiz != NULL ){
+
+		cout << pg->claves[1].Codigo << " ";
+
+		// llamada recursiva por la primera hoja
+		this->preOrdenRecursivoAux(pg->ramas[0]);
+
+		if(pg->ramas[0] != NULL){
+			// llamada recursiva por la primera hoja
+			this->preOrdenRecursivoAux(pg->ramas[1]);
+		}
+
+		for(int i = 2; i <= pg->cuentas; i++){
+
+			cout << pg->claves[i].Codigo  << " ";
+			this->preOrdenRecursivoAux(pg->ramas[i]);
+		}
+	}
+
+}
+
+//fstream file7("PostOrRecBmas.txt", ios::out); //archivo salida de recorrido
+
+void FractalTree::postOrdenRecursivo(Node *raiz){
+	postOrdenRecursivoAux(raiz);
+}
+
+/* Realiza un recorrido del arbol de forma recursiva */
+void FractalTree::postOrdenRecursivoAux(Node *raiz){
+
+	Node *pg = raiz;
+
+	if( raiz != NULL ){
+
+		// llamada recursiva por la primera hoja
+		this->postOrdenRecursivoAux(pg->ramas[0]);
+
+		if(pg->ramas[0] != NULL){
+			// llamada recursiva por la primera hoja
+			this->postOrdenRecursivoAux(pg->ramas[1]);
+		}
+
+		cout << pg->claves[1].Codigo << " ";
+
+		for(int i = 2; i <= pg->cuentas; i++){
+			this->postOrdenRecursivoAux(pg->ramas[i]);
+			cout << pg->claves[i].Codigo  << " ";
+
+		}
+	}
+}
+/**************************************************************/
+
+//fstream file8("PreOrIteBmas.txt", ios::out); //archivo salida de recorrido
+
+void FractalTree::preOrdenIterativo(Node *raiz){
+	preOrdenIterativoAux(raiz);
+}
+
+/* Realiza un recorrido del arbol de forma iterativo */
+void FractalTree::preOrdenIterativoAux(Node *raiz){
+	Node *pg = raiz;
+
+	if( raiz != NULL ){
+
+		cout << pg->claves[1].Codigo << " ";
+
+		// llamada recursiva por la primera hoja
+		this->preOrdenIterativoAux(pg->ramas[0]);
+
+		if(pg->ramas[0] != NULL){
+				// llamada recursiva por la primera hoja
+				this->preOrdenIterativoAux(pg->ramas[1]);
+		}
+
+		for(int i = 2; i <= pg->cuentas; i++){
+
+			cout << pg->claves[i].Codigo  << " ";
+			this->preOrdenIterativoAux(pg->ramas[i]);
+		}
+	}
+
+}
+
+//fstream file9("PostOrIteBmas.txt", ios::out); //archivo salida de recorrido
+
+void FractalTree::postOrdenIterativo(Node *raiz){
+	postOrdenIterativoAux(raiz);
+}
+
+/* Realiza un recorrido del arbol de forma iterativo */
+void FractalTree::postOrdenIterativoAux(Node *raiz){
+	Node *pg = raiz;
+
+	if( raiz != NULL ){
+
+		// llamada recursiva por la primera hoja
+		this->postOrdenIterativoAux(pg->ramas[0]);
+
+		if(pg->ramas[0] != NULL){
+			// llamada recursiva por la primera hoja
+			this->postOrdenIterativoAux(pg->ramas[1]);
+		}
+
+		cout << pg->claves[1].Codigo << " ";
+
+		for(int i = 2; i <= pg->cuentas; i++){
+			this->postOrdenIterativoAux(pg->ramas[i]);
+			cout << pg->claves[i].Codigo  << " ";
+		}
+	}
+}
+
+
--- Logic/FractalTree.h
+++ Logic/FractalTree.h
@@ -17,16 +17,84 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _F_TREE_H_
-#define _F_TREE_H_
+#ifndef _FRACTALTREE_H_
+#define _FRATREE_H_
 
-class FTree
+class FractalTree
 {
 public:
+	Node *raiz, *Xr, *P, *N, *vsam;
+	int k, cuentas, MAX, MIN, nivel;
+	NodoProducto mediana;
+	bool esta, ea;
+	double contT, contTot;
+	int eli;
+	int arreglo[50];
+	//bandera es del archivo maestro, y bandera2
+    int bandera, bandera2;
+	//Prototipos
+	FractalTree(){
+		cuentas = k = 0;
+		esta    = ea = false;
+		MAX     = 4;
+		MIN     = 2;
+		nivel   =0;
+		vsam    = raiz = P = N = Xr = NULL;
+		contT= contTot=bandera = bandera2 = eli =0;
+	}
+	void refrescar ();
+	void insertar (NodoProducto);
+	void empujar(NodoProducto, Node *);
+	void meterHoja();
+	bool arbolVacio(Node *);
+	void dividir ();
+	void doblar (Node *);
+	//imprime el arbol en orden por la referencia de la raiz
+	void print_rec ();
+	void print_rec_aux (Node *nodo);//auxiliar
+	bool buscarNodo (NodoProducto clave, Node *nodo);
+	bool buscar (NodoProducto clave, Node *nodo);
+	//busca si una clace existe o no en el arbol
+	bool buscar (NodoProducto clave);
+	//da la altura
+	int  Altura ();
+	//imprime en anchura
+	void Anchura();
+	void Anchura_aux (Node *, int , int);
+	//imprime por niveles auxiliar de anchura aux
+	void ImprimeNivel(Node *, int);
+	//imprime por la referencia de orden
+	void Imprimir ();
+	//carga el archivo de productos
+	void Cargar();
+
+	/***********  Recorridos **************************/
+
+	void postOrdenRecursivo(Node *r);
+	void postOrdenIterativo(Node *r);
+	void preOrdenRecursivo(Node *r);
+	void preOrdenIterativo(Node *r);
+
+	void postOrdenRecursivoAux(Node *r);
+	void postOrdenIterativoAux(Node *r);
+	void preOrdenRecursivoAux(Node *r);
+	void preOrdenIterativoAux(Node *r);
+
+	//bool buscarNodo (int clave, Node *pagina);
+	bool Buscar (int clave);
+	//elimina al archivo
+	void Eliminar(int);
+	//ve si c esta en el archivo
+	int Esta(int c);
+	NodoProducto* Encontrar(int c);
+
+	bool EstaArreglo(int num);
+	void ImprimirArreglo();
 
 protected:
 
 private:
+	 
 
 };
 
